bf3518bc60aa51c5bf635dd2a07953eb93d85f26
Paul Cochrane <paul@liekut.de>
2014-06-18 20:24:39 +0200

    [features] using nose.tools for better assert statements

--- features/steps/hotzen_spotz_is_run.py
+++ features/steps/hotzen_spotz_is_run.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from behave import given, then
+from nose.tools import assert_equal
 from subprocess import Popen, PIPE
 
 @given(u'the program hotzen_spotz is run')
@@ -10,14 +11,14 @@ def run_hotzen_spotz(context):
             close_fds=True)
     context.stdout, context.stderr = process.communicate()
 
-    assert process.returncode == 0
+    assert_equal(process.returncode, 0)
 
 @then(u'I should see the welcome screen')
 def see_welcome_screen(context):
     output = context.stdout.strip().split('\n')[:2]
     welcome_message = context.text.strip().split('\n')[:2]
 
-    assert output[0] == welcome_message[0]
-    assert output[1] == welcome_message[1]
+    assert_equal(output[0], welcome_message[0])
+    assert_equal(output[1], welcome_message[1])
 
 # vim: expandtab shiftwidth=4 softtabstop=4
