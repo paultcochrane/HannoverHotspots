f5f1ae9a353e7e2dc88be90e5c4ef49a87b1f4b6
Paul Cochrane <paul@liekut.de>
2014-06-19 10:56:01 +0200

    [features] retooling tests to use pexpect instead of subprocess

--- features/steps/hotzen_spotz_is_run.py
+++ features/steps/hotzen_spotz_is_run.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from behave import given, then
-from nose.tools import assert_equal, assert_regexp_matches
-from subprocess import Popen, PIPE
+from nose.tools import assert_false, assert_equal, assert_regexp_matches
+import pexpect
 import re
 
 @given(u'the program hotzen_spotz is run')
 def hotzen_spotz_is_run(context):
-    process = Popen("bin/hotzen_spotz", shell=True,
-            stdin=PIPE, stdout=PIPE, stderr=PIPE,
-            close_fds=True)
-    process.stdin.write('exit')
-    context.stdout, context.stderr = process.communicate()
+    process = pexpect.spawn("python -u bin/hotzen_spotz")
 
-    assert_equal(process.returncode, 0)
+    context.process = process
+    assert_false(process.eof())
 
 @then(u'I should see the welcome screen')
 def see_welcome_screen(context):
-    output = context.stdout.strip().split('\n')[:2]
     welcome_message = context.text.strip().split('\n')[:2]
+    output = context.process.readline().strip()
 
-    assert_equal(output[0], welcome_message[0])
-    assert_equal(output[1], welcome_message[1])
+    assert_equal(output, welcome_message[0])
+
+    output = context.process.readline().strip()
+    assert_equal(output, welcome_message[1])
 
 @then(u'I should see the command prompt')
 def see_command_prompt(context):
-    output = context.stdout.strip()
+    output = context.process.readline().strip()
+
+    assert_equal(output, "")
+
+    context.process.sendline("")  # flush the command line
+    output = context.process.readline().strip()
     regexp = re.compile(".*%s.*" % context.text, re.MULTILINE)
 
     assert_regexp_matches(output, regexp)
