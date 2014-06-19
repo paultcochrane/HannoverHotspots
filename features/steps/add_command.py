7cc2e96beebdbf49c3a7ea0fc5a299f6b0e09c7e
Paul Cochrane <paul@liekut.de>
2014-06-19 18:04:13 +0200

    [features] adding the add command steps

---
+++ features/steps/add_command.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from behave import when, then
+from nose.tools import assert_true, assert_equal
+
+@when(u'I enter "add"')
+def i_enter_add(context):
+    bytes_sent = context.process.sendline("add")
+    assert_equal(bytes_sent, 4)
+
+    context.process.sendline("") # flush
+    assert_true(context.process.isalive())
+
+@then(u'I should be asked to add an entry')
+def the_program_exits(context):
+    add_prompt_text = "Please enter cafe/bar's name:"
+    output = context.process.readline()
+    assert_equal(output, add_prompt_text)
+
+# vim: expandtab shiftwidth=4 softtabstop=4
