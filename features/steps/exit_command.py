e10698a72600020fa9977788ea2f4516fcdd505b
Paul Cochrane <paul@liekut.de>
2014-06-19 17:26:35 +0200

    [features] implementing the exit_command test steps

--- features/steps/exit_command.py
+++ features/steps/exit_command.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from behave import when, then
+from nose.tools import assert_false, assert_equal
 
 @when(u'I enter "exit"')
 def i_enter_exit(context):
-    assert False
+    bytes_sent = context.process.sendline("exit")
+    assert_equal(bytes_sent, 5)
+
+    context.process.sendline("") # flush
+    assert_false(context.process.isalive())
 
 @then(u'the program should exit cleanly')
 def the_program_exits(context):
-    assert False
+    assert_equal(context.process.exitstatus, 0)
 
 # vim: expandtab shiftwidth=4 softtabstop=4
