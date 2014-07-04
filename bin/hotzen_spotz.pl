497b37aa595692c92b096e80cecc52956494a717
Paul Cochrane <paul@liekut.de>
2014-07-04 16:11:20 +0200

    Wrapping main code in a sub
    This means that we can localise the variables much better.

--- bin/hotzen_spotz.pl
+++ bin/hotzen_spotz.pl
@@ -1,18 +1,22 @@
 #!/usr/bin/env perl
- 
+
 use warnings;
 use strict;
- 
-use IO::Prompt;
- 
-print "Welcome to the Hannover Hotspot editor, version 0.1\n";
-print "Use Ctrl+D to exit, type 'help' or '?' for help.\n";
- 
-my $prompt = "spotz_editor> ";
-while( prompt $prompt ) {
-    exit(0) if $_ eq "exit";
 
-    print $_, "\n";
+use IO::Prompt;
+
+sub hotzen_spots {
+    print "Welcome to the Hannover Hotspot editor, version 0.1\n";
+    print "Use Ctrl+D to exit, type 'help' or '?' for help.\n";
+
+    my $prompt = "spotz_editor> ";
+    while( prompt $prompt ) {
+        exit(0) if $_ eq "exit";
+
+        print $_, "\n";
+    }
 }
+
+hotzen_spots() unless caller();
 
 # vim: expandtab shiftwidth=4 softtabstop=4
