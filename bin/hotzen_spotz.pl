38a19c836d80acb42f0be5f0845fbde7ccb103ea
Paul Cochrane <paul@liekut.de>
2014-07-04 16:20:02 +0200

    Adding POD docs to the main program

--- bin/hotzen_spotz.pl
+++ bin/hotzen_spotz.pl
@@ -1,10 +1,49 @@
 #!/usr/bin/env perl
 
+=head1 NAME
+
+hotzen_spotz - add, edit and delete HannoverHotspots entries
+
+=head1 SYNOPSIS
+
+    $ hotzen_spotz
+    spotz_editor> add
+    ...
+    spotz_editor> edit
+    ...
+    spotz_editor> delete
+    ...
+
+=head1 DESCRIPTION
+
+Starts an interactive shell with commands to add, edit and delete entries in
+the list of WLAN hotspots in Hannover, Germany.
+
+=head1 AUTHOR
+
+Paul Cochrane <paul@liekut.de>
+
+=head1 COPYRIGHT AND LICENSE
+
+This program is free software; you may redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
 use warnings;
 use strict;
 use autodie;
+use Pod::Usage;
 
 use IO::Prompt;
+
+=over 4
+
+=item hotzen_spotz()
+
+Run the main command loop.
+
+=cut
 
 sub hotzen_spots {
     print "Welcome to the Hannover Hotspot editor, version 0.1\n";
@@ -19,5 +58,9 @@ sub hotzen_spots {
 }
 
 hotzen_spots() unless caller();
+
+=back
+
+=cut
 
 # vim: expandtab shiftwidth=4 softtabstop=4
