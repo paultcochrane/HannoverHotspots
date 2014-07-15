7ab1807a4a05b2886a91ceb6337ed4d7f371670d
Paul Cochrane <paul@liekut.de>
2014-07-15 17:08:53 +0200

    [bin] Moving add_entry() into HotspotsEditor() class

--- lib/HotspotsEditor.pm
+++ lib/HotspotsEditor.pm
@@ -1,6 +1,7 @@
 package HotspotsEditor;
 
 use Moose;
+use IO::Prompt;
 
 =item exit_program()
 
@@ -10,6 +11,60 @@ Exit the hotspots editor program.
 
 sub exit_program {
     exit(0);
+}
+
+=item add_entry()
+
+Add a WLAN hotspot entry.
+
+=cut
+
+sub add_entry {
+    my $prompt = "Please enter the location's name: ";
+    my $location_name = prompt("-p" => $prompt, -raw_input);
+
+    $prompt = "Please enter location's type (cafe, bar, restaurant): ";
+    my $location_type = prompt("-p" => $prompt, -raw_input);
+
+    $prompt = "Please enter location's SSID: ";
+    my $location_ssid = prompt("-p" => $prompt, -raw_input);
+
+    $prompt = "Is WLAN free here? (yes/no): ";
+    my $is_wlan_free = prompt("-p" => $prompt, -yes_no);
+
+    $prompt = "Please enter the location's street address: ";
+    my $location_street_address = prompt("-p" => $prompt, -raw_input);
+
+    $prompt = "Please enter the location's URL: ";
+    my $location_url = prompt("-p" => $prompt, -raw_input);
+
+    $prompt = "Please enter the location's latitude (in degrees): ";
+    my $location_latitude = prompt("-p" => $prompt, -num);
+
+    $prompt = "Please enter the location's longitude (in degrees): ";
+    my $location_longitude = prompt("-p" => $prompt, -num);
+
+    $prompt = "Please enter notes about location's power points: ";
+    my $power_points_notes = prompt("-p" => $prompt, -raw_input);
+
+    $prompt = "Please enter notes about location's network speed: ";
+    my $network_speed_notes = prompt("-p" => $prompt, -raw_input);
+
+    $prompt = "Please enter extra notes about the location: ";
+    my $extra_notes = prompt("-p" => $prompt, -raw_input);
+
+    print "Entry information:\n";
+    print "    Location name: ",       $location_name, "\n";
+    print "    Location type: ",       $location_type, "\n";
+    print "    Location SSID: ",       $location_ssid, "\n";
+    print "    Free WLAN?: ",          $is_wlan_free, "\n";
+    print "    Street address: ",      $location_street_address, "\n";
+    print "    Location URL: ",        $location_url, "\n";
+    print "    Location latitude: ",   $location_latitude, "\n";
+    print "    Location longitude: ",  $location_longitude, "\n";
+    print "    Power points notes: ",  $power_points_notes, "\n";
+    print "    Network speed notes: ", $network_speed_notes, "\n";
+    print "    Extra notes: ",         $extra_notes, "\n";
 }
 
 1;
