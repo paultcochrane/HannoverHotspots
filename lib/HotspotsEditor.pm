96d9d085d1c01528e27ab113f3838e21e4210d56
Paul Cochrane <paul@liekut.de>
2014-07-15 17:06:36 +0200

    [bin] exit_program() is now a method of HotspotsEditor()

--- lib/HotspotsEditor.pm
+++ lib/HotspotsEditor.pm
@@ -2,6 +2,16 @@ package HotspotsEditor;
 
 use Moose;
 
+=item exit_program()
+
+Exit the hotspots editor program.
+
+=cut
+
+sub exit_program {
+    exit(0);
+}
+
 1;
 
 # vim: expandtab shiftwidth=4 softtabstop=4
