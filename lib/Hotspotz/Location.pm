f81f135246b8625c12e8bd19eed133c6b8393bfb
Paul Cochrane <paul@liekut.de>
2014-07-23 13:48:39 +0200

    Location objects can now print themselves

--- lib/Hotspotz/Location.pm
+++ lib/Hotspotz/Location.pm
@@ -22,6 +22,29 @@ has \@attrs => (
     default => '',
 );
 
+=item print
+
+Display a location's information
+
+=cut
+
+sub print {
+    my $self = shift;
+
+    print "Name: ",               $self->name, "\n";
+    print "Type: ",               $self->type, "\n";
+    print "SSID: ",               $self->ssid, "\n";
+    print "Free WLAN?: ",         $self->is_wlan_free, "\n";
+    print "Street address: ",     $self->street_address, "\n";
+    print "URL: ",                $self->url, "\n";
+    print "Coordinates: ",        $self->longitude, "E, ",
+                                  $self->latitude, "N", "\n";
+    print "Power points info: ",  $self->power_points_notes, "\n";
+    print "Network speed info: ", $self->network_speed_notes, "\n";
+    print "Extra info: ",         $self->extra_notes, "\n";
+    print "Last update: ",        $self->last_update, "\n";
+}
+
 1;
 
 # vim: expandtab shiftwidth=4 softtabstop=4
