1e6ab5bb840464ef1e28d159489f0e352577222c
Paul Cochrane <paul@liekut.de>
2014-08-04 19:02:51 +0200

    [features] removing unnecessary context array variable

--- features/step_definitions/add_command_steps.pl
+++ features/step_definitions/add_command_steps.pl
@@ -210,7 +210,7 @@ Then qr/^I should see an overview of the entered information$/, func($context) {
 
 When qr/^I add a new entry$/, func($context) {
     my $spotz = $context->stash->{'scenario'}->{'object'};
-    $context->stash->{'scenario'}->{'add_entry_overview'} = [];
+    my @add_entry_overview = [];
     $spotz->send("add\n");
 
     my $prompt = "Please enter the location's name:";
@@ -222,77 +222,75 @@ When qr/^I add a new entry$/, func($context) {
 
     my $answer = "cafe";
     $spotz->send("$answer\n");
-    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+    push @add_entry_overview, $answer;
 
     $prompt = "Please enter location's SSID:";
     $spotz->expect(1, $prompt);
 
     $answer = "CafeWLAN";
     $spotz->send("$answer\n");
-    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+    push @add_entry_overview, $answer;
 
     $prompt = "Is WLAN free here? (yes/no):";
     $spotz->expect(1, $prompt);
 
     $answer = "yes";
     $spotz->send("$answer\n");
-    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+    push @add_entry_overview, $answer;
 
     $prompt = "Please enter the location's street address:";
     $spotz->expect(1, $prompt);
 
     $answer = "Unbekannter Weg 1";
     $spotz->send("$answer\n");
-    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+    push @add_entry_overview, $answer;
 
     $prompt = "Please enter the location's URL:";
     $spotz->expect(1, $prompt);
 
     $answer = "http://cafewlan.de";
     $spotz->send("$answer\n");
-    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+    push @add_entry_overview, $answer;
 
     $prompt = "Please enter the location's latitude (in degrees):";
     $spotz->expect(1, $prompt);
 
     $answer = 50.123456;
     $spotz->send("$answer\n");
-    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+    push @add_entry_overview, $answer;
 
     $prompt = "Please enter the location's longitude (in degrees):";
     $spotz->expect(1, $prompt);
 
     $answer = 9.654321;
     $spotz->send("$answer\n");
-    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+    push @add_entry_overview, $answer;
 
     $prompt = "Please enter notes about location's power points:";
     $spotz->expect(1, $prompt);
 
     $answer = "Upstairs, in the corner";
     $spotz->send("$answer\n");
-    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+    push @add_entry_overview, $answer;
 
     $prompt = "Please enter notes about location's network speed:";
     $spotz->expect(1, $prompt);
 
     $answer = "Good; 500kbs download; 200kbs upload";
     $spotz->send("$answer\n");
-    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+    push @add_entry_overview, $answer;
 
     $prompt = "Please enter extra notes about the location:";
     $spotz->expect(1, $prompt);
 
     $answer = "SSH blocked; HTTP(S) allowed";
     $spotz->send("$answer\n");
-    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+    push @add_entry_overview, $answer;
 
     my $message = "Entry information:";
     my $index = $spotz->expect(1, $message);
     is($index, 1, "Entry information overview");
 
-    my @add_entry_overview =
-        @{$context->stash->{'scenario'}->{'add_entry_overview'}};
     print join "\n", @add_entry_overview;
     for my $entry ( @add_entry_overview ) {
         $index = $spotz->expect(1, $entry);
