fcb0f31294431ff7a28bee54375750b6e48b25a5
Paul Cochrane <paul@liekut.de>
2014-08-04 11:21:27 +0200

    [features] implementing first draft of "add new entry" feature step

--- features/step_definitions/add_command_steps.pl
+++ features/step_definitions/add_command_steps.pl
@@ -208,4 +208,96 @@ Then qr/^I should see an overview of the entered information$/, func($context) {
     }
 };
 
+When qr/^I add a new entry$/, func($context) {
+    my $spotz = $context->stash->{'scenario'}->{'object'};
+    $context->stash->{'scenario'}->{'add_entry_overview'} = [];
+    $spotz->send("add\n");
+
+    my $prompt = "Please enter the location's name:";
+    $spotz->expect(1, $prompt);
+    $spotz->send("Cafe with WLAN\n");
+
+    $prompt = "Please enter location's type (cafe, bar, restaurant):";
+    $spotz->expect(1, $prompt);
+
+    my $answer = "cafe";
+    $spotz->send("$answer\n");
+    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+
+    $prompt = "Please enter location's SSID:";
+    $spotz->expect(1, $prompt);
+
+    $answer = "CafeWLAN";
+    $spotz->send("$answer\n");
+    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+
+    $prompt = "Is WLAN free here? (yes/no):";
+    $spotz->expect(1, $prompt);
+
+    $answer = "yes";
+    $spotz->send("$answer\n");
+    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+
+    $prompt = "Please enter the location's street address:";
+    $spotz->expect(1, $prompt);
+
+    $answer = "Unbekannter Weg 1";
+    $spotz->send("$answer\n");
+    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+
+    $prompt = "Please enter the location's URL:";
+    $spotz->expect(1, $prompt);
+
+    $answer = "http://cafewlan.de";
+    $spotz->send("$answer\n");
+    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+
+    $prompt = "Please enter the location's latitude (in degrees):";
+    $spotz->expect(1, $prompt);
+
+    $answer = 50.123456;
+    $spotz->send("$answer\n");
+    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+
+    $prompt = "Please enter the location's longitude (in degrees):";
+    $spotz->expect(1, $prompt);
+
+    $answer = 9.654321;
+    $spotz->send("$answer\n");
+    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+
+    $prompt = "Please enter notes about location's power points:";
+    $spotz->expect(1, $prompt);
+
+    $answer = "Upstairs, in the corner";
+    $spotz->send("$answer\n");
+    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+
+    $prompt = "Please enter notes about location's network speed:";
+    $spotz->expect(1, $prompt);
+
+    $answer = "Good; 500kbs download; 200kbs upload";
+    $spotz->send("$answer\n");
+    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+
+    $prompt = "Please enter extra notes about the location:";
+    $spotz->expect(1, $prompt);
+
+    $answer = "SSH blocked; HTTP(S) allowed";
+    $spotz->send("$answer\n");
+    push $context->stash->{'scenario'}->{'add_entry_overview'}, $answer;
+
+    my $message = "Entry information:";
+    my $index = $spotz->expect(1, $message);
+    is($index, 1, "Entry information overview");
+
+    my @add_entry_overview =
+        @{$context->stash->{'scenario'}->{'add_entry_overview'}};
+    print join "\n", @add_entry_overview;
+    for my $entry ( @add_entry_overview ) {
+        $index = $spotz->expect(1, $entry);
+        is($index, 1, "Entry overview: $entry");
+    }
+};
+
 # vim: expandtab shiftwidth=4 softtabstop=4
