cd2621d5621feb9156ba802317335b3db9ff9383
Paul Cochrane <paul@liekut.de>
2014-08-05 13:52:39 +0200

    [features] refactoring spotz process extraction into a sub in load command steps

--- features/step_definitions/load_command_steps.pl
+++ features/step_definitions/load_command_steps.pl
@@ -8,26 +8,31 @@ use Method::Signatures;
 use Expect;
 
 Given qr/^I have explicitly loaded a location file$/, func($context) {
-    my $spotz = $context->stash->{'scenario'}->{'object'};
+    my $spotz = spotz($context);
     create_test_json_file();
     $spotz->send("load test.json\n");
     is($spotz->match_number(), 1, "Load command entry");
 };
 
 When qr/^I enter \"load\" with an existing filename$/, func($context) {
-    my $spotz = $context->stash->{'scenario'}->{'object'};
+    my $spotz = spotz($context);
     create_test_json_file();
     $spotz->send("load test.json\n");
     is($spotz->match_number(), 1, "Load command entry");
 };
 
 Then qr/^I should see the entries in the loaded file$/, func($context) {
-    my $spotz = $context->stash->{'scenario'}->{'object'};
+    my $spotz = spotz($context);
     my $index = $spotz->expect(1, "Lindens Centrum");
     is($index, 1, "Display entry from explicitly loaded file");
     $index = $spotz->expect(1, "Cafe Spandau");
     is($index, 1, "Display entry from explicitly loaded file");
 };
+
+sub spotz {
+    my $context = shift;
+    return $context->stash->{'scenario'}->{'object'};
+}
 
 sub create_test_json_file {
     open my $fh, ">", "test.json" or die "$!";
